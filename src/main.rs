ab4e6085651052db174ebd26870d19f4e7456440
Prathamesh Patil - AirFi <prathamesh.patil@airfi.aero>
2025-03-30 13:18:50 +0530

    interpolation & positional arguments to println!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    let apples = 10;
+    let oranges = 20;
+    let fruits = apples + oranges;
+    println!("This year, my garden has {} apples.", apples); // Interpolated Arguments
+    println!("This year, my graden has total {fruits} fruits"); // Interpolated Arguments
+    println!(
+        "This year, my garden has {0} apples, {1} oragnes and total {2} fruits",
+        apples, oranges, fruits
+    ); // Positional Arguments
 }

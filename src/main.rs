38a7e96a200a4d909edbee785c83eb2f8e9a7756
Prathamesh Patil - AirFi <prathamesh.patil@airfi.aero>
2025-04-01 10:07:20 +0530

    integers

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
-    println!("Hello, world!");
+    /* 
+        Signed Integer
+            - Support +ve and -ve values. They start with with an i(integer)
+    */
+
+    let eight_bit: i32 = 8;
+    let sixteen_bit: i16 = 16;
+    let thirty_two_bit: i32 = 32;
+
+
+    /* 
+        Unsigned Integer
+            - Support +ve values. They start with with an u(unsigned)
+            - Only support zero & positive values.
+            - They can store a larger max value in positive direction.
+    */
+
 }

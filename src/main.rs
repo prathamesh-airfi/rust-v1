1d21f1f0bff3c226ed0a8287cd2a18ab92413fd8
Prathamesh Patil - AirFi <prathamesh.patil@airfi.aero>
2025-04-06 19:42:58 +0530

    references and borrowing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hello, world!");
+    let name = "Hello There!";
+    let name_ref = &name;
+    let name_ref_1 = &name_ref;
+    let name_ref_2 = &name_ref_1;
+
+    /* Manual Dereferencing */
+    println!("{}", *name_ref);
+
+    /* Automatic Dereferencing */
+    println!("{}", name_ref_2);
+
+    /* Actual Address Printing */
+    println!("{:p}", name);
+    println!("{:p}", name_ref);
+    println!("{:p}", name_ref_1);
+    println!("{:p}", name_ref_2);
 }

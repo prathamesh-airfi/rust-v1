3a63be0e3c41a3ebc6a18e583e0877d2e6b7fa67
Prathamesh Patil - AirFi <prathamesh.patil@airfi.aero>
2025-04-09 11:45:15 +0530

    project

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,29 @@
 fn main() {
-    let mut name = String::from("Prathamesh");
-    let ref_1 = &mut name;
-    let ref_2 = &name;
-    println!("{ref_2}");
+    let mut trip = start_trip();
+    visit_philadelphia(&mut trip);
+    trip.push_str(" and ");
+    visit_new_york(&mut trip);
+    trip.push_str(" and ");
+    visit_boston(&mut trip);
+    show_itinerary(&trip);
+}
+
+fn start_trip() -> String {
+    String::from("The plan is...")
+}
+
+fn visit_philadelphia(text: &mut String) {
+    text.push_str("Philadelphia");
+}
+
+fn visit_new_york(text: &mut String) {
+    text.push_str("New York");
+}
+
+fn visit_boston(text: &mut String) {
+    text.push_str("Boston.");
+}
+
+fn show_itinerary(text: &String) {
+    println!("{text}");
 }

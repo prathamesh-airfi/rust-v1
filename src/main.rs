200253fea2f51ffadcbdd256bcd92e5dc68c8270
Prathamesh Patil - AirFi <prathamesh.patil@airfi.aero>
2025-05-25 09:08:30 +0530

    project

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,33 @@
 fn main() {
-    let mut my_arr = [10, 15, 20, 25, 30];
-    let my_slice = &mut my_arr[2..4];
-    println!("{:?}", my_slice);
-    my_slice[0] = 21;
-    println!("{:?}", my_slice);
+    let mut cereals: [String; 5] = [
+        String::from("Cookie Crisp"),
+        String::from("Cinnamon Toast Crunch"),
+        String::from("Frosted Flakes"),
+        String::from("Cocoa Puffs"),
+        String::from("Captain Crunch"),
+    ];
+
+    let first_two = &cereals[..2];
+    println!("{:?}", first_two);
+
+    let mid_three = &cereals[1..4];
+    println!("{:?}", mid_three);
+
+    let last_three = &mut cereals[2..5];
+    println!("{:?}", last_three);
+
+    last_three[2] = String::from("Lucky Charms");
+    println!("{:?}", cereals);
+
+
+    let cookie_crisp = &mut cereals[0];
+    let cookie = &cookie_crisp[0..=5];
+    println!("{}", cookie);
+
+    let cocoa_puffs = &cereals[3];
+    println!("{cocoa_puffs}");
+
+    let puffs = &cocoa_puffs[6..];
+    println!("{}", puffs);
+
 }

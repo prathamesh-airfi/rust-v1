43dbab73d38f83609c45680389d3e03ab2737d0b
Prathamesh Patil - AirFi <prathamesh.patil@airfi.aero>
2025-05-25 08:40:28 +0530

    mutable array slices

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,7 @@
 fn main() {
-    let action_hero = String::from("Arnold Schwarzenegger");
-    let string_slice = &action_hero[0..6]; // Slicing with length of bytes, 1 byte = 1 char, except special chars
-    println!("{string_slice}");
-
-    print_str_length(string_slice);
-    print_str_length(&action_hero); // Deref Coercion
-
-    let arr: [i32; 5] = [1, 2, 3, 4, 5];
-    let arr_slice = &arr[..3];
-
-    print_array_length(&arr);
-    print_array_length(&arr_slice);
-
-}
-
-fn print_str_length(text: &str) {
-    println!("String slice has length of {}", text.len());
-}
-
-fn print_array_length(arr: &[i32]) {
-    println!("Array slice has size of {}", arr.len());
+    let mut my_arr = [10, 15, 20, 25, 30];
+    let my_slice = &mut my_arr[2..4];
+    println!("{:?}", my_slice);
+    my_slice[0] = 21;
+    println!("{:?}", my_slice);
 }

9f45adc29d847b690d037e8871e6b8f823b23bc4
Prathamesh Patil - AirFi <prathamesh.patil@airfi.aero>
2025-05-26 12:14:18 +0530

    struct-definition

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+/* Blueprint */
+#[derive(Debug)]
+struct Coffee {
+    price: f64,
+    name: String,
+    is_hot: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    /* Struct Instance */
+    let mocha = make_coffee(String::from("Mocha"), 12.99, true);
+
+    println!(
+        "My {} this morning cost {}. It is {} that is was hot",
+        mocha.name, mocha.price, mocha.is_hot
+    );
+
+    drink_coffee(&mocha);
+    println!("{:?}", mocha);
+    println!("{:#?}", mocha);
+}
+
+fn make_coffee(name: String, price: f64, is_hot: bool) -> Coffee {
+    Coffee {
+        name,
+        price,
+        is_hot,
+    }
+}
+
+fn drink_coffee(coffee: &Coffee) {
+    println!("Drinking my delicious {}", coffee.name);
 }

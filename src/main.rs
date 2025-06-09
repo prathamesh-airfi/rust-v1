53c93e6b9f6143786c724ab2e70ae2e19427a77a
Prathamesh Patil - AirFi <prathamesh.patil@airfi.aero>
2025-06-09 11:50:05 +0530

    defining methods on enum

--- src/main.rs
+++ src/main.rs
@@ -4,21 +4,23 @@ enum LaundryCycle {
     Delicate(String),
 }
 
-fn main() {
-    wash_laundry(LaundryCycle::Cold);
-    wash_laundry(LaundryCycle::Hot { temperature: 14 });
-    wash_laundry(LaundryCycle::Delicate("Cotton".to_string()));
-}
+impl LaundryCycle {
+    fn wash_laundry(&self) {
+        match self {
+            LaundryCycle::Cold => println!("Running the laundry with cold temperature"),
+            LaundryCycle::Hot { temperature } => {
+                println!("Running the laundry with temperature of {temperature}")
+            }
 
-fn wash_laundry(cycle: LaundryCycle) {
-    match cycle {
-        LaundryCycle::Cold => println!("Running the laundry with cold temperature"),
-        LaundryCycle::Hot { temperature } => {
-            println!("Running the laundry with temperature of {temperature}")
-        }
-
-        LaundryCycle::Delicate(fabric_type) => {
-            println!("Running the laundry with delicate cycle for {fabric_type}")
+            LaundryCycle::Delicate(fabric_type) => {
+                println!("Running the laundry with delicate cycle for {fabric_type}")
+            }
         }
     }
+}
+
+fn main() {
+    LaundryCycle::Cold.wash_laundry();
+    LaundryCycle::Hot { temperature: 14 }.wash_laundry();
+    LaundryCycle::Delicate("Cotton".to_string()).wash_laundry();
 }

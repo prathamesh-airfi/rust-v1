9996c2a3f885ead41bfff9b4a46c39fcb090aacd
Prathamesh Patil - AirFi <prathamesh.patil@airfi.aero>
2025-06-09 11:38:07 +0530

    match keyword II

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,10 @@ fn main() {
     let my_computer = OperatingSystem::Linux;
     let age = years_since_releases(my_computer);
     println!("My computer's operating system is {age} years old");
+
+    let dads_computer = OperatingSystem::MacOS;
+    let age = years_since_releases(dads_computer);
+    println!("Dad's computer's operating system is {age} years old");
 }
 
 fn years_since_releases(os: OperatingSystem) -> u32 {

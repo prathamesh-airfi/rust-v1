7a9afca7307553665ce47deb1ef620ab2816d4e7
Prathamesh Patil - AirFi <prathamesh.patil@airfi.aero>
2025-06-09 13:08:00 +0530

    project

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,33 @@
 #[derive(Debug)]
-enum Milk {
-    Lowfat(i32),
-    Whole,
-    NonDairy { kind: String },
+enum Tier {
+    Gold,
+    Silver,
+    Platinum,
+}
+
+#[derive(Debug)]
+enum Subscription {
+    Free,
+    Basic(f64, u32),
+    Premium { tier: Tier },
+}
+
+impl Subscription {
+    fn summarize(&self) {
+        match self {
+            Subscription::Free => println!("You have limited access to the site"),
+            Subscription::Basic(price, months) => println!(
+                "You have limited access to the site's premium features for {price} for {months} months"
+            ),
+            Subscription::Premium { tier } => println!(
+                "You have full access to the site's premium features. Your tier is {tier:?}"
+            ),
+        }
+    }
 }
 
 fn main() {
-    let my_beverage = Milk::Whole;
-
-    let Milk::Lowfat(percent) = my_beverage else {
-        println!("I got other kind of milk");
-        return;
-    };
-
-    println!("I've got {percent} percent of milk");
+    Subscription::Free.summarize();
+    Subscription::Basic(12.0, 2).summarize();
+    Subscription::Premium { tier: Tier::Gold }.summarize();
 }

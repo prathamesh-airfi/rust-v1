01999480e328fcb23e4420187e4266836c33b7bb
Prathamesh Patil - AirFi <prathamesh.patil@airfi.aero>
2025-06-16 11:55:00 +0530

    generics with enum

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,13 @@
 #[derive(Debug)]
-struct TreasureChest<T> {
-    captain: String,
-    treasure: T,
-}
-
-impl TreasureChest<String> {
-    fn clean_treasure(&mut self) {
-        self.treasure = self.treasure.trim().to_string();
-    }
-}
-impl TreasureChest<[&str; 3]> {
-    fn amount_of_treasure(&self) -> usize {
-        self.treasure.len()
-    }
-}
-
-impl<T> TreasureChest<T> {
-    fn capital_captain(&mut self) {
-        self.captain = self.captain.to_uppercase();
-    }
+enum CheeseSteak<T> {
+    Plain,
+    Topping(T),
 }
 
 fn main() {
-    let mut gold_chest = TreasureChest {
-        captain: "Firebeard".to_string(),
-        treasure: "Gold",
-    };
+    let mushroom: CheeseSteak<String> = CheeseSteak::<String>::Topping(String::from("Mushroom"));
+    let plain_cheese_steak = CheeseSteak::<String>::Plain;
 
-    gold_chest.capital_captain();
-    println!("{:#?}", gold_chest);
-
-    let mut silver_chest = TreasureChest {
-        captain: "Bloodsail".to_string(),
-        treasure: "Silver".to_string(),
-    };
-
-    silver_chest.capital_captain();
-    silver_chest.clean_treasure();
-    println!("{:#?}", silver_chest);
-
-    let mut special_chest = TreasureChest {
-        captain: "Bootyplunder".to_string(),
-        treasure: ["Gold", "Silver", "Platinum"],
-    };
-
-    special_chest.capital_captain();
-    special_chest.amount_of_treasure();
-    println!("{:#?}", special_chest);
+    println!("{:?}", mushroom);
+    println!("{:?}", plain_cheese_steak);
 }

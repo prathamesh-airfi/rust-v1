b194b6a0ea449b1920f786c71c29d75b71840e90
Prathamesh Patil - AirFi <prathamesh.patil@airfi.aero>
2025-06-16 10:46:00 +0530

    single & multiple generics

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    println!("{}", identity::<i32>(12));
+    println!("{:?}", make_tuple::<&str, i32>("Hello", 24));
+}
+
+fn identity<T>(val: T) -> T {
+    val
+}
+
+fn make_tuple<T, Q>(first: T, second: Q) -> (T, Q) {
+    return (first, second);
 }

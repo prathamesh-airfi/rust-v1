2580ae29b66247a6cf2aa3870fc1255329579cf1
Prathamesh Patil - AirFi <prathamesh.patil@airfi.aero>
2025-06-16 11:43:06 +0530

    structs with generics

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,50 @@
+#[derive(Debug)]
+struct TreasureChest<T> {
+    captain: String,
+    treasure: T,
+}
+
+impl TreasureChest<String> {
+    fn clean_treasure(&mut self) {
+        self.treasure = self.treasure.trim().to_string();
+    }
+}
+impl TreasureChest<[&str; 3]> {
+    fn amount_of_treasure(&self) -> usize {
+        self.treasure.len()
+    }
+}
+
+impl<T> TreasureChest<T> {
+    fn capital_captain(&mut self) {
+        self.captain = self.captain.to_uppercase();
+    }
+}
+
 fn main() {
-    println!("{}", identity::<i32>(12));
-    println!("{:?}", make_tuple::<&str, i32>("Hello", 24));
-}
+    let mut gold_chest = TreasureChest {
+        captain: "Firebeard".to_string(),
+        treasure: "Gold",
+    };
 
-fn identity<T>(val: T) -> T {
-    val
-}
+    gold_chest.capital_captain();
+    println!("{:#?}", gold_chest);
 
-fn make_tuple<T, Q>(first: T, second: Q) -> (T, Q) {
-    return (first, second);
+    let mut silver_chest = TreasureChest {
+        captain: "Bloodsail".to_string(),
+        treasure: "Silver".to_string(),
+    };
+
+    silver_chest.capital_captain();
+    silver_chest.clean_treasure();
+    println!("{:#?}", silver_chest);
+
+    let mut special_chest = TreasureChest {
+        captain: "Bootyplunder".to_string(),
+        treasure: ["Gold", "Silver", "Platinum"],
+    };
+
+    special_chest.capital_captain();
+    special_chest.amount_of_treasure();
+    println!("{:#?}", special_chest);
 }

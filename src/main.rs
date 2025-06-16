bfd410823da0b006f300ff5cdb05ee760e8d96f1
Prathamesh Patil - AirFi <prathamesh.patil@airfi.aero>
2025-06-16 12:49:12 +0530

    project

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,46 @@
 #[derive(Debug)]
-enum CheeseSteak<T> {
-    Plain,
-    Topping(T),
+enum DigitalContent {
+    AudioFile,
+    VideoFile,
+}
+
+#[derive(Debug)]
+struct ChatMessage<T> {
+    content: T,
+    time: String,
+}
+
+impl ChatMessage<DigitalContent> {
+    fn consume_entertainment(&self) {
+        println!("Watching the {:?}", self.content);
+    }
+}
+
+impl<T> ChatMessage<T> {
+    fn retrieve_time(&self) -> String {
+        self.time.clone()
+    }
 }
 
 fn main() {
-    let mushroom: CheeseSteak<String> = CheeseSteak::<String>::Topping(String::from("Mushroom"));
-    let plain_cheese_steak = CheeseSteak::<String>::Plain;
+    let message_1 = ChatMessage::<&str> {
+        content: "Hello Dear",
+        time: String::from("12:00 am"),
+    };
 
-    println!("{:?}", mushroom);
-    println!("{:?}", plain_cheese_steak);
+    let message_2 = ChatMessage::<String> {
+        content: String::from("Get Lost you bastard"),
+        time: String::from("12:01 am"),
+    };
+
+    let audio_message = ChatMessage::<DigitalContent> {
+        content: DigitalContent::AudioFile,
+        time: String::from("12:02 am"),
+    };
+
+    audio_message.consume_entertainment();
+
+    println!("{}", message_1.retrieve_time());
+    println!("{}", message_2.retrieve_time());
+    println!("{}", audio_message.retrieve_time());
 }

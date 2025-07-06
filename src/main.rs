b19283920d5f3f4df1c98e715c34dc87a40b2fdd
Prathamesh Patil - AirFi <prathamesh.patil@airfi.aero>
2025-07-06 21:57:30 +0530

    project

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,52 @@
+#[derive(Debug)]
+struct Food {
+    name: String,
+}
+
+#[derive(Debug)]
+struct Restaurant {
+    reservations: u32,
+    has_mice_infestation: bool,
+}
+
+impl Restaurant {
+    fn chef_special(&self) -> Option<Food> {
+        match self.has_mice_infestation {
+            true => None::<Food>,
+            false if self.reservations > 12 => Some(Food {
+                name: "Strip Steak".to_string(),
+            }),
+            false => Some(Food {
+                name: "Uni Sashimi".to_string(),
+            }),
+        }
+    }
+
+    fn deliver_burger(&self, addr: &str) -> Result<Food, String> {
+        if self.has_mice_infestation {
+            return Err(String::from("Sorry, we have a mice problem"));
+        }
+        match addr.is_empty() {
+            true => Err(String::from("No delivery address specified")),
+            false => Ok(self.chef_special().unwrap()),
+        }
+    }
+}
+
 fn main() {
-    let optional_value = Option::Some(12);
-    let none_value = None::<i32>;
+    let restaurant_1 = Restaurant {
+        has_mice_infestation: true,
+        reservations: 11,
+    };
 
-    let ok_value = Result::<bool, String>::Ok(true);
-    let err_value = Result::<bool, String>::Err("dfdf".to_string());
+    println!("{:?}", restaurant_1.chef_special());
+    println!("{:?}", restaurant_1.deliver_burger("123 Elm Street"));
 
-    println!("{:#?}", optional_value);
-    println!("{:#?}", none_value);
-    println!("{:#?}", ok_value);
-    println!("{:#?}", err_value);
+    let restaurant_2 = Restaurant {
+        has_mice_infestation: false,
+        reservations: 15,
+    };
+    println!("{:?}", restaurant_2.chef_special());
+    println!("{:?}", restaurant_2.deliver_burger(""));
+    println!("{:?}", restaurant_2.deliver_burger("123 Elm Street"));
 }

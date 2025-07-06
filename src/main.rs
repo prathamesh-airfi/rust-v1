2a56d82a5f9592debd5c885ed79112419ddfc039
Prathamesh Patil - AirFi <prathamesh.patil@airfi.aero>
2025-07-06 21:32:22 +0530

    options and result enum

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    let optional_value = Option::Some(12);
+    let none_value = None::<i32>;
+
+    let ok_value = Result::<bool, String>::Ok(true);
+    let err_value = Result::<bool, String>::Err("dfdf".to_string());
+
+    println!("{:#?}", optional_value);
+    println!("{:#?}", none_value);
+    println!("{:#?}", ok_value);
+    println!("{:#?}", err_value);
 }

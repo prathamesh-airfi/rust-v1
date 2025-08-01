9af2d373a3dc0b284663e723782051aa97eef7f6
Prathamesh Patil - AirFi <prathamesh.patil@airfi.aero>
2025-08-01 12:29:08 +0530

    creating vectors

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+#[allow(unused_variables)]
+
 fn main() {
-    println!("Hello, world!");
+    let pizza_diameters: Vec<i32> = Vec::new();
+    let pizza_diameters = Vec::<i32>::new();
+    println!("{:?}", pizza_diameters);
+
+    let pastas = Vec::<String>::new();
+    println!("{:?}", pastas);
+
+    // Direct value assignment
+    let pizza_diameters = vec![8, 10, 12, 14];
+    println!("{:?}", pizza_diameters);
 }

8f8b370965bcc35168700b1fa42be6c5ea250850
Prathamesh Patil - AirFi <prathamesh.patil@airfi.aero>
2025-08-04 19:49:45 +0530

    adding or removing element from vector

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,18 @@
 #[allow(unused_variables)]
 
 fn main() {
-    let pizza_diameters: Vec<i32> = Vec::new();
-    let pizza_diameters = Vec::<i32>::new();
+    let mut pizza_diameters = vec![8, 10, 12, 14];
+    /* Add element to last */
+    pizza_diameters.push(16);
+    pizza_diameters.push(18);
+
+    /* Add element to specific index */
+    pizza_diameters.insert(0, 6);
     println!("{:?}", pizza_diameters);
 
-    let pastas = Vec::<String>::new();
-    println!("{:?}", pastas);
+    /* Remove element from last index */
+    let val = pizza_diameters.pop();
 
-    // Direct value assignment
-    let pizza_diameters = vec![8, 10, 12, 14];
-    println!("{:?}", pizza_diameters);
+    /* Remove element from specific index */
+    let val = pizza_diameters.remove(1);
 }

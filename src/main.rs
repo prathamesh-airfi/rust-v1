1e23745dd6f9c8b28afda0e918060a6ce5b56091
Prathamesh Patil - AirFi <prathamesh.patil@airfi.aero>
2025-08-05 22:23:44 +0530

    project

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,48 @@
 #[allow(unused_variables)]
+#[derive(Debug)]
+struct File {
+    name: String,
+}
+
+#[derive(Debug)]
+struct Folder {
+    name: String,
+    contents: Vec<File>,
+}
+
+impl Folder {
+    fn new(name: String) -> Self {
+        Self {
+            name,
+            contents: vec![],
+        }
+    }
+
+    fn crete_file(&mut self, name: String) {
+        self.contents.push(File { name });
+    }
+
+    fn delete_file(&mut self, index: usize) -> File {
+        self.contents.remove(index)
+    }
+
+    fn get_file(&self, index: usize) -> Option<&File> {
+        self.contents.get(index)
+    }
+}
 
 fn main() {
-    let pepperoni = "Pepperoni".to_string();
-    let mushroom = "Mushroom".to_string();
-    let sausages = "Sausages".to_string();
-    let mut pizza_toppings = vec![pepperoni, mushroom, sausages];
+    let mut music_folder = Folder::new("Music".to_string());
 
-    pizza_toppings[1] = "Mast Mushroom".into();
-    pizza_toppings.push(String::from("Extra Cheese"));
+    music_folder.crete_file("in the name of love.mp3".to_string());
+    music_folder.crete_file("ignite".to_string());
+
+    music_folder.delete_file(0);
+
+    println!("{:?}", music_folder);
+
+    match music_folder.get_file(0) {
+        Some(file) => println!("{file:#?}"),
+        None => println!("There was no file"),
+    }
 }
